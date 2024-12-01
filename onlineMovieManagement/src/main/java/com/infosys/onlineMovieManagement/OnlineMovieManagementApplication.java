4cedd06c4ea97bd8575d0edd0e7ac3dc52c72874
SouravBanerje <sourav2473486@gmail.com>
2024-12-01 20:11:37 +0530

    First Version

---
+++ onlineMovieManagement/src/main/java/com/infosys/onlineMovieManagement/OnlineMovieManagementApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.onlineMovieManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieManagementApplication.class, args);
+	}
+
+}
